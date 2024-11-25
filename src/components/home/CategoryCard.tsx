6bb5e94ec836ec1298199b9067ee4def3985c24c
delap <de.la.parra82@hotmail.com>
2024-11-24 21:47:25 -0600

    [newsBanner] Apply the bootstrap style to navbar and change props name

--- src/components/home/CategoryCard.tsx
+++ src/components/home/CategoryCard.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-interface propsWithChildren {
+interface Card {
     title: string;
     image: string;
     imageAlt: string;
 }
 
-const CategoryCard = ({ title, image, imageAlt }: propsWithChildren): JSX.Element => {
+const CategoryCard = ({ title, image, imageAlt }: Card): JSX.Element => {
     return (
         <div className="category-card">
             <picture>
-                <source srcSet={`/images/${image}.avif`} type='image/avif' />
-                <source srcSet={`/images/${image}.webp`} type='image/webp' />
-                <img src={`/images/${image}.png`} alt={imageAlt} className='category-image' />
+                <img src={image} alt={imageAlt} className='category-image' />
             </picture>
             <div className="category-title">
                 <h3>{title}</h3>
